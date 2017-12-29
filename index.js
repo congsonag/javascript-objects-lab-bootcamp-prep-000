71c34e86a254f6a4618fa482275ab06e5892710c
Stephane <son.trinh.dev@gmail.com>
2017-12-29 04:23:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var cloneObject = Object.assign( {}, object);
-  cloneObject
+  delete cloneObject.key;
+  return cloneObject
 }
 console.log(deleteFromObjectByKey(recipes, 1))
