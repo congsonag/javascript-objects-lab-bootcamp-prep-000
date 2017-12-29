df3e00b728fa11d2b2583c54bb239dbb272a4801
Stephane <son.trinh.dev@gmail.com>
2017-12-29 04:24:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ function deleteFromObjectByKey(object, key) {
   delete cloneObject.key;
   return cloneObject
 }
-console.log(deleteFromObjectByKey(recipes, breakfast))
+console.log(deleteFromObjectByKey(recipes, 'breakfast'))
